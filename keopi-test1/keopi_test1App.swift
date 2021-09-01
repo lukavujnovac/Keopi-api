0f4718c6e03890729c4badbbd8cc156894e4d955
Luka <vujnovacluka89@gmail.com>
2021-09-01 18:26:21 +0200

    Initial Commit

---
+++ keopi-test1/keopi_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  keopi_test1App.swift
+//  keopi-test1
+//
+//  Created by Luka Vujnovac on 01.09.2021..
+//
+
+import SwiftUI
+
+@main
+struct keopi_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
